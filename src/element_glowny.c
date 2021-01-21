a4682878b7fda328a8ede8ba187f02ddb98e6213
Paulinka-M <ktost84@gmail.com>
2021-01-21 19:31:53 +0100

    dodac uzycie w pliku gauss

---
+++ src/element_glowny.c
@@ -0,0 +1,39 @@
+#include "mat_io.h"
+#include "element_glowny.h"
+#include "gauss.h"
+
+#include <math.h>
+#include <stdlib.h>
+//wybor niezerowego elementu diagonalnego
+
+int wybor(int k, Matrix *mat, Matrix *b)
+{//w - wiersz, k- kolumna
+	int max = fabs(mat->data[k][k]);
+	int w = k;
+	
+	int i = k + 1, j = 0;
+	double pom;
+	for(i; i < mat -> r; i ++)  // petla szuka max
+		for(j=i;j<mat->r;j++)
+			if(max < fabs(mat->data[j][i]))
+			{
+				max = mat->data[j][i];
+				w = j;//zmieniam nr wiersza ktory wybrany
+			}
+	//zamien wiersze
+	if (max > fabs(mat->data[i][i])){
+		for(i;i < mat->c; i++)
+		{
+			pom = mat->data[w][i];
+			mat->data[w][i] = mat->data[k][i];
+			mat->data[k][i] = pom;
+		}
+		pom = b->data[w][0];
+		b->data[w][0] = b->data[k][0];
+		b->data[k][0] = pom;
+	}
+	if(mat->data[k][k] == 0 ) 
+		return -1; //nie udalo sie zamienic
+	else 
+		return 0;
+}
